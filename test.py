96b16d93d800c03783936da295d932ed980dc85b
theheavygluon <45721650+theheavygluon@users.noreply.github.com>
2021-01-21 01:32:09 +0400

    First version of test.py and 3 example solutions
    -test.py now acts as a mini-documentation which displays a few example features. 
    
    -ProbaPlot.png is a plot of the probability of finding a particle described by psi = cos(pi*x) between -1/2 and x
    
    -BlackbodyPlot.png is the plot of the power distribution for blackbodies with the following temperatures: 5000, 5500, 6500
    
    -HarmOsc.mp4 is an animation of the potential function, first 3 eigenvalues and first 3 Re[psi] and Im[psi] for a particle bound to V(x) = 0.5*x^2 (egeinvalues are [0.5, 1.5, 2.5])

--- test.py
+++ test.py
@@ -1,7 +1,38 @@
-from QPy import psiTools as psi
+#Demo of some features
+
 from QPy import *
+from QPy import Solver as QSolve
+from QPy import Plotter as QPlot 
+from QPy import tise
+from QPy import psiTools as psi 
 
-def wf(x):
-    return np.sin(PI*x)
-a = psi.normalize(wf, 0.5, 0.5)
+#QSolve feature example: Solving f = (KE + phi)/h for KE = 6000, phi = 5000 
+print(QSolve.photoelectric.frequency(6000,5000))
 
+#QPlot feature example: Plotting the Spectral Radiance vs Wavelength(~Intensity) for temps 5000,5500,6500 using default domain
+QPlot.blackBody([5000,5500,6500])
+
+#Getting the first 3 solutions of the TISE for the harmonic oscillator (note that tise.harmOsc(1,3) would give the exact same result)
+tise.solve("0.5*x**2", 3)
+
+#Normalizing psi(x) = x*e^(-x^2)
+print(psi.normalize(lambda x: x*exp(-x**2)))
+
+#Normalizing the wavefunction solution of the infinite cubic well where l = 1, n = 1 
+#(Since l = 1, the x,y,z boundaries are +/- l/2 or +/- 0.5)
+
+print(psi.normalize(lambda x,y,z: sin(PI*x)*sin(PI*y)*sin(PI*z), -0.5, 0.5, -0.5, 0.5, -0.5, 0.5))
+
+
+#Plotting the probability of finding a particle defined by psi(x) = cos(PI*x) between it's left barrier (-0.5) and x 
+
+x = [i for i in np.linspace(-0.5,0.5, 10000)] 
+y = [psi.prob(lambda x: cos(PI*x), -0.5,i, -0.5, 0.5) for i in x]
+
+plt.plot(x,y, color='r', label='$\int_{-0.5}^{x} |\psi(x)^2|$, where $\psi = \cos(\pi x)$')
+
+plt.title('Plotting the probability density of finding the particle between -0.5 and x \n (Because its normalized, $Prob_{(-0.5,0.5) = 1}$)')
+plt.legend()
+plt.xlabel('$x$')
+plt.ylabel('$Prob_{(-0.5,x)}$')
+plt.show()
